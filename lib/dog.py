ce8e80d16a5ad4b2220604258519ec468d44a793
Maderu-CM <clive.moyia@student.moringaschool.com>
2023-09-09 01:05:55 +0300

    Assignment Done

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,38 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name, breed):
+        self._name = None
+        self._breed = None
+        self.name = name  # Use the property setter
+        self.breed = breed  # Use the property setter
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in the list of approved breeds.")
+
+if __name__ == "__main__":
+    fido = Dog("Fido", "Beagle")
+    print(fido.name)  # Should print "Fido"
+    print(fido.breed)  # Should print "Beagle"
+
+    rocky = Dog("Rocky", "German Shepherd")  # Should print an error message
+    rocky.name = "Rocky the Great Dane with a Very Long Name"  # Should print an error message
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,39 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name, job):
+        self._name = None
+        self._job = None
+        self.name = name  # Use the property setter
+        self.job = job  # Use the property setter
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) < 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string under 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in the list of approved jobs.")
+
+if __name__ == "__main__":
+    alice = Person("Alice Smith", "Marketing")
+    print(alice.name)  # Should print "Alice Smith" in title case
+    print(alice.job)  # Should print "Marketing"
+
+    bob = Person("Bob Johnson", "Engineering")  # Should print an error message
+    bob.name = "Robert Johnson with a Very Long Name"  # Should print an error message
+
